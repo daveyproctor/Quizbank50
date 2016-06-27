38e651dcc2c3848c89a6452a9f00f2e873852655
David Proctor <david.proctor@yale.edu>
2016-06-27 18:25:49 +0000

    first commit porting over to including broader framework. might be garbage even now

--- public/question.php
+++ public/question.php
@@ -1,16 +1,17 @@
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "GET")
     {
-        //return("hello");
+        $array["key"] = file_get_contents("../uploads" . "question.html");
+        //print(json_encode(["key" => "value"]));
+        print(json_encode($array));
+    }
+    
+    //scrap
+            //return("hello");
         // $questions_array;
         // $questions_array[0] = file_get_contents("question.html");
         // return(JSON_encode($questions_array));
         
         // print(file_get_contents("question.html"));
         // print("hi");
-        
-        $array["key"] = file_get_contents("question.html");
-        //print(json_encode(["key" => "value"]));
-        print(json_encode($array));
-    }
 ?>
