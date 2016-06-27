c2b5a9f9c1e857e9e751ffbc23634616eac07c02
David Proctor <david.proctor@yale.edu>
2016-06-27 23:29:41 +0000

    working even better!

--- bin/import.php
+++ bin/import.php
@@ -4,6 +4,8 @@
     define("SUBJECT_INDEX", 6);
     //so max index is ten
     define("LINE_END", 11);
+    //number of questions you want to insert
+    define("END", 10);
     
     //helper function to insert question's several tags into tag table.
     function tag_insert($row, $question_id)
@@ -11,6 +13,11 @@
         $index = SUBJECT_INDEX;
         while ($index < LINE_END)
         {
+            if (empty($row[$index]))
+            {
+                print("this question didn't have full 5 tags");
+                return true;
+            }
             $insertion = CS50::query("INSERT INTO subject_tags (question_id, tag) VALUES (?,?)", $question_id, $row[$index]);
             if ($insertion === false)
             {
@@ -48,13 +55,17 @@
     //exit(0);
     
      //(($array = fgetcsv($fp, 1000, "\t")) != FALSE) 
-    while (($row = fgetcsv($handle, 1000, "\t")) == FALSE)
+    while (($row = fgetcsv($handle, 1000, "\t")) != FALSE && $question_id < END)
     {
         $row = explode(',', $row[0]);
         $boolean = CS50::query("INSERT IGNORE INTO questions (Year, Quiz_num, Question_num) VALUES (?,?,?)", $row[0], $row[1], $row[2]);
-        if($boolean)
+        if(!$boolean)
         {
-            print("this worked\n");
+            print("insertion into questions table failed\n");
+        }
+        if (!tag_insert($row, $question_id))
+        {
+            print("could not insert into tags table");
         }
         
         
