95843cdea1ec479d0f2185b81d2e9bce5aca7319
David Proctor <david.proctor@yale.edu>
2016-06-28 02:06:15 +0000

    getting really close; just need to render object correctly. bug was due to not requireing config.php

--- public/tags.php
+++ public/tags.php
@@ -1,9 +1,11 @@
 <?php
-    if ($_SERVER["REQUEST_METHOD"] == "GET")
-    {
-        $query_start = $_GET['start'] . '%';
-        $tags = CS50::query("SELECT DISTINCT tag FROM subject_tags WHERE tag like ?", $start);
-        if (!tags)
+    require("../includes/config.php");
+    // if ($_SERVER["REQUEST_METHOD"] == "GET")
+    // {
+        //$query_start = $_GET['start'] . '%';
+        $query_start = "s%";
+        $tags = CS50::query("SELECT DISTINCT tag FROM subject_tags WHERE tag like ?", $query_start);
+        if (!$tags)
         {
             $array = ["key1" => "tag1", "key2" => "tag2"];
             print(json_encode($array));
@@ -12,5 +14,5 @@
         {
             print(json_encode($tags));
         }
-    }
+    //}
 ?>
