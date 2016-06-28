a520f41da7842fb698fd7c5d41c8b5d6222ce649
David Proctor <david.proctor@yale.edu>
2016-06-28 02:08:36 +0000

    typeahead.0 is workinggit add --all need to add .change() listener to update it as you type

--- public/tags.php
+++ public/tags.php
@@ -1,9 +1,9 @@
 <?php
     require("../includes/config.php");
-    // if ($_SERVER["REQUEST_METHOD"] == "GET")
-    // {
-        //$query_start = $_GET['start'] . '%';
-        $query_start = "s%";
+    if ($_SERVER["REQUEST_METHOD"] == "GET")
+    {
+        $query_start = $_GET['start'] . '%';
+        //$query_start = "s%";
         $tags = CS50::query("SELECT DISTINCT tag FROM subject_tags WHERE tag like ?", $query_start);
         if (!$tags)
         {
@@ -14,5 +14,5 @@
         {
             print(json_encode($tags));
         }
-    //}
+    }
 ?>
