37903d1feb56c8542428344735ec1a79fbe0175c
David Proctor <david.proctor@yale.edu>
2016-06-28 01:46:12 +0000

    basic ajax is working; all's left is to sort out the sql query for typeahead

--- public/tags.php
+++ public/tags.php
@@ -1,7 +1,8 @@
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "GET")
     {
-        $tags = CS50::query("SELECT DISTINCT tags FROM subject_tags WHERE ")
+        //$tags = CS50::query("SELECT DISTINCT tags FROM subject_tags WHERE ")
+        $array = ["key1" => "tag1", "key2" => "tag2"];
         print(json_encode($array));
     }
 ?>
